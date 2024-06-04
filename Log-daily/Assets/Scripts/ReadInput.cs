340929ee1f064f4ec2254bad0db08940fa84f8d5
Hieu <zoro331@gmail.com>
2024-06-04 00:09:43 -0400

    add smoothness and field check

--- Log-daily/Assets/Scripts/ReadInput.cs
+++ Log-daily/Assets/Scripts/ReadInput.cs
@@ -1,15 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ReadInput : MonoBehaviour
 {
+    [SerializeField] Button printButton;
     string nameInput, companyInput, personVisitingInput, purposeInput;
     bool isUsInput, isConsentInput;
+    bool isEmpty = false;
+
+    private Color disableColor = Color.grey;
+    private Color enableColor = Color.white;
+    
     public void GetNameInput(string name)
     {
         nameInput = name;
         Debug.Log(nameInput);
+        
     }
 
     public void CompanyInput(string company)
@@ -42,5 +50,43 @@ public class ReadInput : MonoBehaviour
         Debug.Log(purposeInput);
     }
 
+    private void Start()
+    {
+        nameInput = string.Empty;
+        companyInput = string.Empty;
+
+        printButton.enabled = false;
+        printButton.gameObject.GetComponent<Image>().color = disableColor;
+    }
+
+    private void Update()
+    {
+        InputCheck();
+    }
+
+    private void InputCheck()
+    {
+        if (isConsentInput && !EmptyStringCheck(nameInput) && !EmptyStringCheck(companyInput))
+        {
+            printButton.gameObject.GetComponent<Image>().color = enableColor;
+            printButton.enabled = true;
+        }
+
+        else
+        {
+            printButton.gameObject.GetComponent<Image>().color = disableColor;
+            printButton.enabled = false;
+        }
+    }
+
+    public bool EmptyStringCheck(string s)
+    {
+        return s == string.Empty;
+
+
+    }
+
+
+   
 
 }
